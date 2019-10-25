d2a38135d03e96fe0489775b70839b0f5a92bb8c
Leon <52199211+bboyGit@users.noreply.github.com>
2019-10-25 13:05:38 +0800

    Update perceptron.py
    Added the dual method of perceptron.

--- core/supervise/classification/perceptron.py
+++ core/supervise/classification/perceptron.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def perceptron(x, y, init, alpha, max_iter, method='dual'):
+def perceptron(x, y, init, alpha, max_iter, method='original'):
     """
     Desc: Execute perceptron algorithm to do binary classification
     Parameters:
@@ -10,8 +10,8 @@ def perceptron(x, y, init, alpha, max_iter, method='dual'):
       init: An column vector contain the initial guess of w and b.
       alpha: An float representing learning rate used in stochastic gradient descent
       max_iter: An int indicating the maximum iteration times
-      method: An str either 'dual' or 'normal'
-    Return: The coefficients of perception function
+      method: An str either 'dual' or 'original'
+    Return: A tuple containing the coefficients of perception function and iterate times.
     Note:
         Perceptron is a kind of linear binary classifier which classify samples by a fitted hyperplane(x*w + b = 0).
         And our goal is to find w and b so that all sample points will be correctly classified by x*w + b = 0.
@@ -27,33 +27,59 @@ def perceptron(x, y, init, alpha, max_iter, method='dual'):
         raise Exception('init must be a column vector')
     count = 0
     N = x.shape[0]
-    one = np.ones([N, 1])
-    x_ = np.concatenate([x, one], axis=1)
-    wb = init.copy()
 
-    while count < max_iter:
-        count += 1
-        Loss = pd.Series([y[i, 0] * (x_[[i], :] @ wb)[0, 0] for i in range(N)])
-        mistake_idx = Loss[Loss <= 10**(-10)].index
-        if len(mistake_idx) == 0:
-            break
-        else:
-            idx = mistake_idx[0]
-            the_x = x_[[idx], :].copy()                        # 选取要进行梯度下降的误分类样本点
-            the_y = y[idx, 0]
-            loss = the_y * (the_x @ wb)
-            while loss <= 10**(-10):
-                wb = wb + alpha * the_y * the_x.T
+    if method == 'original':
+        wb = init.copy()
+        one = np.ones([N, 1])
+        x_ = np.concatenate([x, one], axis=1)
+        f_loss = lambda y_i, x_i, w: y_i * (x_i @ w)[0, 0]
+        while count < max_iter:
+            count += 1
+            Loss = pd.Series([f_loss(y[i, 0], x_[[i], :], wb) for i in range(N)])
+            mistake_idx = Loss[Loss <= 10**(-10)].index
+            if len(mistake_idx) == 0:
+                break
+            else:
+                idx = mistake_idx[0]
+                the_x = x_[[idx], :].copy()                        # 选取要进行梯度下降的误分类样本点
+                the_y = y[idx, 0]
                 loss = the_y * (the_x @ wb)
-    print(count, Loss)
-    converge = False if count >= max_iter else True
-    return wb, converge
+                while loss <= 10**(-10):
+                    wb = wb + alpha * the_y * the_x.T
+                    loss = the_y * (the_x @ wb)
+    elif method == 'dual':
+        n = np.zeros(N)
+        b = 0
+        Gram = x @ x.T
+        f_loss = lambda i, y_i, Gram, n, b: y_i * (b + alpha * sum([n[j] * y[j, 0] * Gram[i, j] for j in range(N)]))
+        while count < max_iter:
+            count += 1
+            Loss = pd.Series([f_loss(i, y[i, 0], Gram, n, b) for i in range(N)])
+            mistake_idx = Loss[Loss <= 10**(-10)].index
+            if len(mistake_idx) == 0:
+                break
+            else:
+                idx = mistake_idx[0]
+                the_y = y[idx, 0]
+                loss = the_y * (b + alpha * sum([n[j] * y[j, 0] * Gram[idx, j] for j in range(N)]))
+                while loss <= 10**(-10):
+                    b = b + alpha * the_y
+                    n[idx] += 1
+                    loss = the_y * (b + alpha * sum([n[j] * y[j, 0] * Gram[idx, j] for j in range(N)]))
+        ny = n.reshape(N, 1) * y
+        w = alpha * x.T @ ny
+        wb = np.concatenate([w, np.array([[b]])])
+
+    return wb, count
 
 if __name__ == "__main__":
     x = np.array([[1, 2], [1.1, 2.1], [0.8, 1.5]])
     y = np.array([[1, 1, -1]]).T
     guess = np.zeros([1, 3]).T
-    perceptron(x, y, init=guess, alpha=0.02, max_iter=1000)
+    percep = perceptron(x, y, init=guess, alpha=0.02, max_iter=1000, method='dual')
+    wb = percep[0].ravel()
+    percep1 = perceptron(x, y, init=guess, alpha=0.02, max_iter=1000, method='original')
+    wb1 = percep1[0].ravel()             # wb1与wb相等，可看出原始型感知机与其对偶型算法结果相同
     import matplotlib.pyplot as plt
     plt.scatter(x[:, 0], x[:, 1])
-    plt.plot(np.linspace(0.7, 1.2, 100), 1.6 - 0.08 * np.linspace(0.7, 1.2, 100))
+    plt.plot(np.linspace(0.7, 1.2, 100), -wb[2]/wb[1] - wb[0]/wb[1] * np.linspace(0.7, 1.2, 100))
