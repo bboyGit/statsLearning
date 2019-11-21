cd724f9d0c44c273edda8d2571c6efca6107c281
Leon <52199211+bboyGit@users.noreply.github.com>
2019-11-21 19:52:00 +0800

    Update gradDesc.py
    调整了梯度下降的代码，使其可以适应矩阵形式的参数。

--- core/optim/gradDesc.py
+++ core/optim/gradDesc.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 import numpy as np
 
 def grad(f, x, delta):
@@ -10,16 +10,16 @@ def grad(f, x, delta):
       delta: A float indicating delta of x
     Return: A column vector containing partial derivatives of each elements
     """
-    grad_vector = []
     fun = f(x)
-    for i, j in enumerate(x):
-        x1 = x.copy()
-        x1[i, :] = j + delta
-        fun1 = f(x1)
-        partial_deriv = (fun1 - fun)/delta
-        grad_vector.append(partial_deriv)
-    grad_vector = np.array([grad_vector]).T
-    return grad_vector
+    nrow, ncol = x.shape
+    grad_ = np.ones([nrow, ncol])
+    for i in range(ncol):
+        for j in range(nrow):
+            x1 = x.copy()
+            x1[j, i] = x1[j, i] + delta
+            grad_[j, i] = f(x1)
+    grad_ = (grad_ - fun)/delta
+    return grad_
 
 def grad_desc(func, x0, tol, max_iter, alpha):
     """
@@ -38,27 +38,26 @@ def grad_desc(func, x0, tol, max_iter, alpha):
     fun = []
     while count < max_iter and diff > tol:
         gradient = grad(f=func, x=x_now, delta=10**(-4))          # Compute gradient of given function in point x_now
-        diff = np.sum(np.abs(gradient)) * alpha
+        diff = np.sqrt(np.sum(gradient**2))
         x_now = x_now - alpha * gradient                          # update point in minus gradient direction
         fun.append(func(x_now))
         count += 1
-
-    result = {'fun': fun, 'x': x_now, 'iter_num': count}
-
+    result = {'x': x_now, 'iter_num': count, 'diff': diff, 'fun': fun}
     return result
 
 
 if __name__ == '__main__':
     # Solve linear regression by gradient descent
-    y = np.array([[2, 1, 5, 9, 7]]).T
-    X = np.array([[0.3, 1, 1.3, 0.6],
-                  [0.45, 1.1, 1, 0.49],
-                  [0.19, 0.89, 0.87, 0.5],
-                  [0.8, 1.7, 1, 0.6],
-                  [0.56, 0.77, 0.4, 0.77]])
+    from sklearn.datasets import load_boston
+    from core.normalize import normalize
+    boston = load_boston()
+    y = boston['target'][:100].reshape(100, 1)
+    X = boston['data'][:100, :3]
+    X = normalize(X).values
+    n = X.shape[1]
     f = lambda x: np.dot((y - X @ x).T, y - X @ x)[0, 0]
-    grad(f, x=np.array([[1, 2.4, 3, 4]], dtype=float).T, delta=0.001)
-    solution = grad_desc(f, np.array([[0, 0, 0, 0]], dtype=float).T, tol=10**(-6), max_iter=3 * 10**4, alpha=0.01)
+    solution = grad_desc(f, np.zeros([n, 1]), tol=10**(-6), max_iter=10**4, alpha=0.001)
     solution['x'].ravel()
+    solution['iter_num']
     import statsmodels.api as sm
     sm.OLS(y, X).fit().params
